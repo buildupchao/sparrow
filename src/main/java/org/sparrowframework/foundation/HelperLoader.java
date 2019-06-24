c3b4fd32cfb2d4b0cf868062b23f2d72a0f5c137
Zychaowill <15536542139@163.com>
2019-06-24 23:51:12 +0800

    finish mvc tools.

--- src/main/java/org/sparrowframework/foundation/HelperLoader.java
+++ src/main/java/org/sparrowframework/foundation/HelperLoader.java
@@ -1,8 +1,8 @@
 package org.sparrowframework.foundation;
 
-import org.sparrowframework.foundation.core.ClassHelper;
 import org.sparrowframework.foundation.ioc.BeanHelper;
 import org.sparrowframework.foundation.ioc.IocHelper;
+import org.sparrowframework.foundation.mvc.ControllerHelper;
 import org.sparrowframework.foundation.util.ClassUtil;
 
 /**
@@ -15,7 +15,8 @@ public final class HelperLoader {
     public static void init() {
         Class<?>[] classList = {
                 BeanHelper.class,
-                IocHelper.class
+                IocHelper.class,
+                ControllerHelper.class
         };
         for (Class<?> cls : classList) {
             ClassUtil.loadClass(cls.getName());
