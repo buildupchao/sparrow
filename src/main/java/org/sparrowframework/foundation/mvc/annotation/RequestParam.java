81e0d90b0987e84b70f654fab05363e85be2c94f
zhangyachao <zhangyachao@wps.cn>
2019-07-03 23:17:20 +0800

    resolve mvc parameter parser function.

---
+++ src/main/java/org/sparrowframework/foundation/mvc/annotation/RequestParam.java
@@ -0,0 +1,23 @@
+package org.sparrowframework.foundation.mvc.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author buildupchao
+ * @date 2019/06/28 09:29
+ * @since JDK 1.8
+ */
+@Documented
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequestParam {
+	String name() default "";
+	
+	boolean required() default true;
+	
+	String defaultValue() default "0";
+}
---
+++ src/main/java/org/sparrowframework/foundation/mvc/converter/ParameterConverter.java
@@ -0,0 +1,11 @@
+package org.sparrowframework.foundation.mvc.converter;
+
+/**
+ * @author buildupchao
+ * @date 2019/06/28 09:40
+ * @since JDK 1.8
+ */
+public interface ParameterConverter {
+
+	void registerConverter();
+}
